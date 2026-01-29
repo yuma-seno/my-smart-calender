ca4cd56b0f4a0f6dc186ab21f37fc5087283cf93
yuma-seno <seno.yu.0816.2003@gmail.com>
2026-01-29 16:36:40 +0000

    feat: fetch時のプロキシにCloudflare Workersを利用するよう修正

--- src/utils/fetchWithProxy.ts
+++ src/utils/fetchWithProxy.ts
@@ -1,11 +1,15 @@
-export const fetchWithProxy = async (url: string): Promise<string> => {
-  const targetUrl = url.includes("?")
+export const fetchWithProxy = async (
+  url: string,
+  withoutProxy = false,
+): Promise<string> => {
+  let targetUrl = url.includes("?")
     ? `${url}&t=${Date.now()}`
     : `${url}?t=${Date.now()}`;
-  const proxyUrl = `https://corsproxy.io/?${encodeURIComponent(targetUrl)}`;
+  if (!withoutProxy)
+    targetUrl = `https://my-smart-calender.seno-yu-0816-2003.workers.dev?url=${encodeURIComponent(targetUrl)}`;
 
   try {
-    const response = await fetch(proxyUrl);
+    const response = await fetch(targetUrl);
     if (!response.ok) throw new Error(`Proxy error: ${response.status}`);
     const text = await response.text();
     if (!text) throw new Error("No content received");
